e53cf12a251ac12de0bba49fa97bb680f80d2dc7
Ashlove32 <52932021+Ashlove32@users.noreply.github.com>
2019-07-15 15:55:23 -0400

    Add files via upload

---
+++ ASHH.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+5/8 #division
+print("Hello World")
+print("I am learning to code on python today")
+print(53)
+(5*2)
+(5/2)
+(5-2)
+(5^2)
+(8/9*3)
+"the division of 5/2 is 2.5"
+print("5/2")
+print("the division of 5/2 is" , 2.5)
+#create fuction to sum any two numbers
+def plus(a,b):
+    plus = a + b
+    print(plus)
+plus(2,3)
+
+#create function to sum any two numbers
+def subtract(a,b):
+    subtract= a-b
+    print(subtract)
+    
+Subtract(80,60)
+
+def triangle(b,h):
+    triangle= b * h * (1/2)
+    print(triangle)
+    
+    triangle(1/2,2,14)
